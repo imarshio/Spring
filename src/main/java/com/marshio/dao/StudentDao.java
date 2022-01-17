ed29f975398908682ae2551a7667dcbf9f1a2f87
marshio <2919029885@qq.com>
2022-01-17 23:11:49 +0800

    2022/01/17 at home on main update

--- src/main/java/com/marshio/dao/StudentDao.java
+++ src/main/java/com/marshio/dao/StudentDao.java
@@ -1,6 +1,9 @@
 package com.marshio.dao;
 
 import com.marshio.pojo.Student;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author masuo
@@ -14,18 +17,46 @@ public interface StudentDao {
     /**
      * 增
      */
-    public int insertStudent(Student student);
+    int insertStudent(Student student);
+
+    int insertStudent2(Student student);
 
     /**
      * 删
      * @param stuNum 学生学号
      */
-    public int deleteStudent(String stuNum);
+    int deleteStudent(String stuNum);
 
     /**
      *
      * @param student 学生信息
      * @return 成功是否
      */
-    public int updateStudent(Student student);
+    int updateStudent(Student student);
+
+    List<Student> listStudents();
+
+    List<Student> listStudents2();
+
+    /**
+     * 在mybatis中进行条件查询，
+     * 如果条件查询方法只有一个简单类型或字符串参数，在mapper中可以直接#{除数字外的任意字符串，建议参数名称}获取
+     * 如果操作方法有一个对象类型参数，在mapper中可以通过#{proName}获取对象指定属性值，proName必须是对象属性名称，如Studeng的stuNum
+     * 如果给两个参数，那么就需要通过如下方法
+     *      1.map参数，在mapper中可以直接通过key获取对应的值
+     *      2.可以通过#{arg0}、#{arg1}、#{param0}、#{param1}取值
+     *      或者通过 @param
+     * @param start 分页开始
+     * @param pageSize 分页大小
+     * @return List<Student>
+     */
+    List<Student> listStudentsByPage(int start, int pageSize);
+
+    List<Student> listStudentsByPage2(@Param("start") int start,
+                                     @Param("pageSize") int pageSize);
+
+    Student queryStudent(String stuNum);
+
+    int getCount();
+
 }
--- src/test/java/com/marshio/dao/StudentDaoTest.java
+++ src/test/java/com/marshio/dao/StudentDaoTest.java
@@ -1,17 +1,15 @@
 package com.marshio.dao;
 
 import com.marshio.pojo.Student;
-import junit.framework.TestCase;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * @author masuo
@@ -35,10 +33,39 @@ public class StudentDaoTest {
 
             StudentDao sd = sqlSession.getMapper(StudentDao.class);
 
-            System.out.println(sd);
-
+            Student student = new Student("20173605", "mas", "1", 20);
             //测试方法
-            int i = sd.insertStudent(new Student("20173602", "mas", "1", 20));
+            int i = sd.insertStudent(student);
+            //此时获取到的主键值为 0
+            System.out.println(student);
+            //手动提交事务，不提交事务不会被完成
+            sqlSession.commit();
+            System.out.println(i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testInsertStudent2() {
+
+        try {
+            //加载配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            //构造器
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            //会话工厂
+            SqlSessionFactory factory = builder.build(is);
+            //会话-连接
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao sd = sqlSession.getMapper(StudentDao.class);
+            Student student = new Student("20173605", "mas", "1", 20);
+            //测试方法
+            int i = sd.insertStudent2(student);
+            //此时返回的学生是附带主键的
+            System.out.println(student);
             //手动提交事务，不提交事务不会被完成
             sqlSession.commit();
             System.out.println(i);
@@ -66,7 +93,7 @@ public class StudentDaoTest {
             System.out.println(sd);
 
             //测试方法
-            int i = sd.deleteStudent("1151511");
+            int i = sd.deleteStudent("20173602");
             //手动提交事务，不提交事务不会被完成
             sqlSession.commit();
             System.out.println(i);
@@ -98,4 +125,125 @@ public class StudentDaoTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testListStudent() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory factory = builder.build(is);
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+            List<Student> students = studentDao.listStudents();
+            for (Student student : students) {
+                System.out.println(student.toString());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testListStudent2() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory factory = builder.build(is);
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+
+            //查询操作不需要提交事务
+            List<Student> students = studentDao.listStudents2();
+            for (Student student : students) {
+                System.out.println(student.toString());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testListStudentsByPage() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory factory = builder.build(is);
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+
+            //查询操作不需要提交事务
+            List<Student> students = studentDao.listStudentsByPage(2,5);
+            for (Student student : students) {
+                System.out.println(student.toString());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testListStudentsByPage2() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory factory = builder.build(is);
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+
+            //查询操作不需要提交事务
+            List<Student> students = studentDao.listStudentsByPage(2,5);
+            for (Student student : students) {
+                System.out.println(student.toString());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testQueryStudent() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory factory = builder.build(is);
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+
+            //查询操作不需要提交事务
+            Student student = studentDao.queryStudent("20173602");
+            System.out.println(student);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetCount() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory factory = builder.build(is);
+            SqlSession sqlSession = factory.openSession();
+
+            StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+
+            //查询操作不需要提交事务
+            int i = studentDao.getCount();
+            System.out.println(i);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
 }
