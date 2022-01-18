5dd9f66ed47aea5110cf6c516a9be851649e4d39
Masuo777 <48401949+Masuo777@users.noreply.github.com>
2022-01-18 14:24:00 +0800

    2022/01/18 at com on main update

--- src/main/java/com/marshio/utils/MybatisUtils.java
+++ src/main/java/com/marshio/utils/MybatisUtils.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 /**
  * @author masuo
  * @data 18/1/2022 下午12:50
- * @Description 封装mybatis工具类
+ * @Description 封装 Mybatis 工具类
  */
 
 public class MybatisUtils {
@@ -32,25 +32,36 @@ public class MybatisUtils {
     }
 
     /**
+     * 手动管理事务
+     *
+     * @return SqlSession
+     */
+    public static SqlSession getSqlSession() {
+        //在线程获取SQL Session
+        return getSqlSession(false);
+    }
+
+    /**
      * 生成SqlSession 的　API接口
      *
      * @return SqlSession实例
      */
-    public static SqlSession getSqlSession() {
+    public static SqlSession getSqlSession(boolean autoCommit) {
         //在线程获取SQL Session
         SqlSession sqlSession = local.get();
         if (sqlSession == null) {
             // 如果为空，就打开一个会话，并放到ThreadLocal
             //这样避免了每次都会打开一个会话，造成资源的浪费。
-            sqlSession = sqlSessionFactory.openSession();
+            sqlSession = sqlSessionFactory.openSession(autoCommit);
             local.set(sqlSession);
         }
         return sqlSession;
     }
 
-    public static <T>T getMapper(Class<T> clazz) {
-        //同样，先尝试从本地线程变量中获取SqlSession
-        SqlSession sqlSession = getSqlSession();
+
+    public static <T> T getMapper(Class<T> clazz) {
+        // 直接调用该方法，表明不管理事务，即自动提交
+        SqlSession sqlSession = getSqlSession(true);
         //然后根据SqlSession获取Mapper对象
         return sqlSession.getMapper(clazz);
     }
